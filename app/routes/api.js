80e1075d4100bc6f6447d089a59edf73acee8f0d
kopolo435 <samirfanilla@gmail.com>
2024-09-22 13:22:43 -0500

    feat: añade acciones de logInventario model

--- app/routes/api.js
+++ app/routes/api.js
@@ -7,6 +7,8 @@ import proveedoresRouter from "./proveedoresRouter.js";
 import unidadMedicamentoRouter from "./unidadMedicamentoRouter.js";
 import invetarioRouter from "./inventarioRouter.js";
 import ventasRouter from "./ventasRouter.js";
+import logventasRouter from "./logVentasRouter.js";
+import logInventarioRouter from "./logInventarioRouter.js";
 
 const router = express.Router();
 
@@ -18,5 +20,7 @@ router.use("/proveedores", proveedoresRouter);
 router.use("/unidad-medicamento", unidadMedicamentoRouter);
 router.use("/inventario", invetarioRouter);
 router.use("/ventas", ventasRouter);
+router.use("/log-inventario", logInventarioRouter);
+router.use("/log-ventas", logventasRouter);
 
 export default router;
